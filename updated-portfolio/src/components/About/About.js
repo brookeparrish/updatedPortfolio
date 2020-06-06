6f8f28cf9c9d9a41cd926b4996db08ed4173206a
Brooke <parrish.e.brooke@gmail.com>
2020-06-05 22:22:07 -0700

    made text smaller and changed about me

--- updated-portfolio/src/components/About/About.js
+++ updated-portfolio/src/components/About/About.js
@@ -6,11 +6,11 @@ function About() {
         <div className="jumbotron jumbotron-fluid text-center jumboBackground pt-2 pb-2 pr-0">
             <div className="container">
                 <h1 className="display-4 name">Brooke Parrish</h1>
-                <p className="lead">I am a technology enthusiast with a certification in Full Stack Web Development. I successfully completed my Bachelor of Arts in Psychology in 2013, but have found my passion in Front-End Developing. I am self-motivated, experienced in multitasking, and have an eagerness to learn and hone my craft.</p>
+                <p className="lead">I am a tech enthusiast with a certification in Full Stack Web Development. I successfully completed my Bachelor of Arts in Psychology in 2013, but have found my passion in Front-End Developing. I am self-motivated, experienced in multitasking, and have an eagerness to learn and hone my craft.</p>
 
                 <p className="lead">I have extensive experience in customer service, working in a group setting and being part of a team where I held an integral role to include a position as Shift Supervisor. The last few years I have gained skills in the dental field where I have conducted new patient consultations and worked in a dental laboratory.</p>
 
-                <p className="lead">When I am not focusing my attention to advancing in the technology world, I love spending time with my family of which I’m the youngest of three siblings, being an aunt to my two nephews, and above all things hold true to the value of family. I also enjoy attending live music events, line dancing, and staying active with various indoor and outdoor activities.</p>
+                <p className="lead">When I am not focusing my attention to advancing in the technology world, I love spending time with my family, being an aunt to my twin nephews, and above all things hold true to the value of family. I also enjoy attending live music events, line dancing, and staying active with various indoor and outdoor activities.</p>
             </div>
         </div>
     );
--- updated-portfolio/src/components/Projects/Projects.js
+++ updated-portfolio/src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
                 <div className="card-deck mx-auto pb-4 pt-4">
                     <div className="card text-center" style={cardStyle}>
                         <div className="card-body">
-                            <p className="card-title bold h2">Events with Corresponding Weather</p>
+                            <p className="card-title bold h3">Events with Corresponding Weather</p>
                             <p className="card-text">This website allows users to search for Eventful's most popular events in a city of their choosing.</p>
                             <a href="https://github.com/brookeparrish/project1" target="_blank" className="card-link">GitHub Repo</a>
                             <a href="https://brookeparrish.github.io/project1/" target="_blank" className="card-link">Visit Website</a>
@@ -21,7 +21,7 @@ function Projects() {
 
                     <div className="card text-center" style={cardStyle}>
                         <div className="card-body">
-                            <p className="card-title font-weight-bolder h2">ScrapRoom</p>
+                            <p className="card-title font-weight-bolder h3">ScrapRoom</p>
                             <p className="card-text">
                                 A live chat app that allows users to "scrap" over topics of their choosing.  This app utilizes socket.io.
                                 <br></br>
@@ -39,7 +39,7 @@ function Projects() {
                 <div className="card-deck mx-auto pb-2">
                     <div className="card text-center" style={cardStyle}>
                         <div className="card-body">
-                            <p className="card-title font-weight-bolder h2">Options App</p>
+                            <p className="card-title font-weight-bolder h3">Options App</p>
                             <p className="card-text">This app is designed to help users learn more about alternative forms of post high school education.
                             <br></br>
                             <br></br>
