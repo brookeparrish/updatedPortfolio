6d59d37a1df69607132a033db1af7733e3aa52b8
Brooke <parrish.e.brooke@gmail.com>
2020-06-05 19:30:39 -0700

    Updates

---
+++ precache-manifest.d53cd21839450b16a48df708c39eb7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9241da8309292ca0a6c917a14114bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855039c096db99100e78",
+    "url": "/static/css/main.016e2280.chunk.css"
+  },
+  {
+    "revision": "74f93676594d0e5b1a49",
+    "url": "/static/js/2.6dcc7d0b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6dcc7d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855039c096db99100e78",
+    "url": "/static/js/main.6ca8fb93.chunk.js"
+  },
+  {
+    "revision": "e6b414bb4b1c1d943c11",
+    "url": "/static/js/runtime-main.0ab961f4.js"
+  }
+]);
